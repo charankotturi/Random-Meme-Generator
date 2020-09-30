68fa7bcc7bf9aabb3cfe6c873ed746473b85b39b
charan <kugelblitz3366@gmail.com>
2020-09-30 09:02:42 +0530

    Volley Library

--- src/main/java/com/example/volleylib/MainActivity.java
+++ src/main/java/com/example/volleylib/MainActivity.java
@@ -65,7 +65,19 @@ public class MainActivity extends AppCompatActivity {
                 shareInfo();
             }
         });
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        this.getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_FULLSCREEN |
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        );
     }
 
     private void shareInfo() {
@@ -75,6 +87,8 @@ public class MainActivity extends AppCompatActivity {
         }
         intent.setType("text/plain");
         startActivity(intent);
+
+        getSupportActionBar().hide();
     }
 
     private void getInfo(){
